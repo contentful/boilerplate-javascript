8ce5550456e1ee276694b58bc04132de7dd6f1d4
Khaled Garbaya <khaled@contentful.com>
2017-01-27 11:57:32 +0100

    Log entry ids instead of json

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ var client = contentful.createClient({
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken: '0b7f6x59a0'
 })
-console.log('\x1b[32mFetching entries ...')
+console.log('\x1b[32m Fetching entries ... \x1b[32m')
 client.getEntries()
   .then((response) => {
-    console.log('\x1b[32mdone.\n')
-    console.log(response.items[0])
+    console.log('\x1b[32m Here are the first 100 entry IDs of your space . \x1b[32m \n')
+    console.log(response.items.map((item) => '> ' + item.sys.id).join('\n'))
   })
   .catch((error) => {
     console.log('\x1b[31merror occured')
